41cb2f25819fc373905ac97a7d6746b5fbc3969b
Andreas Geißinger <geissinger@360-disrupt.de>
2020-02-21 20:15:43 +0100

    update: added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChambersModule } from './chambers/chambers.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ChambersModule],
+  imports: [
+    ChambersModule,
+    MongooseModule.forRoot('mongodb://localhost/LOC_gh')],
   controllers: [AppController],
   providers: [AppService],
 })
