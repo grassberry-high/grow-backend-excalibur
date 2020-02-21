700c7957380aa9a55b1cf478214cf5369b924bb8
Andreas Geißinger <geissinger@360-disrupt.de>
2020-02-21 19:05:16 +0100

    update: added cors for angular

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable cors and setup for angular
+  app.enableCors({
+    credentials: true, origin: true
+  });
+
   await app.listen(3000);
 }
 bootstrap();
