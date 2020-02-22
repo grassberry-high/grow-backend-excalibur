891c732caae0aec6df8b49efaa2aa8074a824abe
Andreas Geißinger <geissinger@360-disrupt.de>
2020-02-22 08:40:22 +0100

    update: added sensors module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChambersModule } from './chambers/chambers.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SensorsModule } from './sensors/sensors.module';
 
 @Module({
   imports: [
     ChambersModule,
-    MongooseModule.forRoot('mongodb://localhost/LOC_gh')],
+    MongooseModule.forRoot('mongodb://localhost/LOC_gh'),
+    SensorsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
