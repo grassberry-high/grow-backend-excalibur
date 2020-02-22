f78e552304dcecbddb946574fcac3fe44ebfaed5
Andreas Geißinger <geissinger@360-disrupt.de>
2020-02-22 08:36:55 +0100

    update: prefix backend routes with backend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // set prefix for routes, for easier nginx int.
+  app.setGlobalPrefix('backend');
+
   // enable cors and setup for angular
   app.enableCors({
     credentials: true, origin: true
