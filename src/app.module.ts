cc17207db5395b3872d54219226ce11fbe857c5f
Omar <moar.ahmed@gmail.com>
2020-03-12 14:59:29 +0100

    feat: generate sensor module,service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChambersModule } from './chambers/chambers.module';
+import {TypegooseModule} from "nestjs-typegoose";
+import { SensorModule } from './sensor/sensor.module';
 
 @Module({
-  imports: [ChambersModule],
+  imports: [
+    ChambersModule,
+    TypegooseModule.forRoot(process.env.MONGO_URI || "mongodb://localhost:27017/test", { useNewUrlParser: true }),
+    SensorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
